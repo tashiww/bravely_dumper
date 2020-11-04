d7677464a37d8ae4a7b6f83b902bed0df1156285
tashi.moe <44418517+tashiww@users.noreply.github.com>
2020-11-03 21:52:26 -0600

    refactoring
    more functions, more reusability

--- bravely_dumper.py
+++ bravely_dumper.py
@@ -5,6 +5,8 @@ import re
 
 # top level directory containing subfolders with script files
 base = Path("A:/content0.game/romfs/common/")
+
+# get list of all files containing text, recursively
 file_list = [f for f in base.rglob('*.*') if f.name != 'index.fs']
 
 # place output file in current script directory
@@ -17,7 +19,7 @@ out = out.open("a")
 
 
 def fprint(text, mode="both", fout=out):
-	""" outputs text to mode: stdout, file, or both """
+	""" outputs text to modes: stdout, file, or both """
 
 	if mode == 'stdout' or mode == 'both':
 		print(text)
@@ -26,22 +28,26 @@ def fprint(text, mode="both", fout=out):
 
 
 def extract_text(src_file, loc, length):
-	""" return a string: length bytes from loc in src_file with enc encoding """
+	""" return a string of length bytes from loc in src_file """
 
 	src_file.seek(loc, 0)
 	text = src_file.read(length).decode("utf-16", "ignore")
 	text = re.sub(r'\x00+', '\n', text)
+
 	return text
 
 
 def read_header(src_file, file_ptr):
-	""" return rel_ptr, abs_ptr, script_len from src_file @ file_ptr location """
+	""" return dict of rel_ptr, abs_ptr, script_len
+		from src_file @ file_ptr location """
+
 	# skip first four bytes of file header
 	src_file.seek(file_ptr+4*4, 0)
 
 	# relative pointers for two text blocks and their lengths
 	names_ptr, names_len, script_ptr, script_len = \
 		struct.unpack("<IIII", src_file.read(16))
+
 	return {
 			"rel_ptr": script_ptr,
 			"script_len": script_len,
@@ -49,53 +55,63 @@ def read_header(src_file, file_ptr):
 			}
 
 
-def find_and_dump(script_path):
-	""" dump text from all files in file_list """
+def read_script(script_path, file_ptr=0, file_name=None):
+	""" parse header and output text from script files,
+		starting at file_ptr offset """
+	with open(script_path, 'rb') as script_file:
+		header = read_header(script_file, file_ptr)
+		if header['script_len'] > 0:
+			# print file path / name starting from /common/
+			fprint("Script File: " + "/".join(script_path.parts[3:]))
 
-	fprint("==="*15)
+			if file_name:
+				fprint(f"internal filename: {file_name}")
+
+			fprint(f"script_location: {header['abs_ptr']}, " +
+										f"script_length: {header['script_len']}")
+			fprint("==="*15)
+			text = extract_text(script_file, header['abs_ptr'], header['script_len'])
+			fprint(text, 'file')
+
+
+def find_and_dump(script_path):
+	""" dump text from file specified by Path object script_path """
+
+	# .fs files contain many files internally, indicated by an index.fs
 	if f.suffix == '.fs':
 		index_path = Path(f.parents[0] / 'index.fs')
 
 		with open(index_path, 'rb') as index_file:
 
-			next_ptr = struct.unpack("<I", index_file.read(4))[0]
-
 			while(True):
 
-				file_ptr, file_len, mystery = struct.unpack("<III", index_file.read(12))
+				next_ptr, file_ptr, file_len, mystery = \
+						struct.unpack("<IIII", index_file.read(16))
+
+				# read to the next pointer, or end of file if no more pointers
 				if next_ptr > 0:
 					name_length = next_ptr - index_file.tell()
 				else:
 					name_length = None
+
+				# file names are UTF-8
 				file_name = index_file.read(name_length).decode()
+				# trim trailing NULLs
 				file_name = re.sub(r'\x00+', '', file_name)
 
-				with open(script_path, 'rb') as script_file:
-					header = read_header(script_file, file_ptr)
-					if header['script_len'] > 0:
-						fprint("Script File: " + "/".join(script_path.parts[3:]))
-						fprint(f"{file_ptr=}, {file_name=}")
-						fprint(f"{header['abs_ptr']=}, {header['script_len']=}")
-						fprint("==="*15)
-						text = extract_text(script_file, header['abs_ptr'], header['script_len'])
-						fprint(text, 'file')
-
+				read_script(script_path, file_ptr, file_name)
 				index_file.seek(next_ptr, 0)
 
+				# exit loop if there are no more pointers in index.fs
 				if next_ptr == 0:
 					break
-				next_ptr = struct.unpack("<I", index_file.read(4))[0]
 
+	# if extension is not .fs, there's no index file to parse
 	else:
-		with open(script_path, 'rb') as script_file:
-			header = read_header(script_file, 0)
-			if header['script_len'] > 0:
-				fprint("Script File: " + "/".join(script_path.parts[3:]))
-				fprint(f"{header['abs_ptr']=}, {header['script_len']=}")
-				fprint("==="*15)
-				text = extract_text(script_file, header['abs_ptr'], header['script_len'])
-				fprint(text, 'file')
+		read_script(script_path)
 
 
+# loop through all script files in /common/
+# parse headers, find text, and output
 for f in file_list:
 	find_and_dump(f)
